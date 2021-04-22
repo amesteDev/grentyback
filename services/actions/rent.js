46ecab8219bf3f7f8c5bc8aba7d76ebf50905f4f
unknown <info@ameste.se>
2021-04-22 13:32:45 +0200

    added RentServ

---
+++ services/actions/rent.js
@@ -0,0 +1,23 @@
+
+
+class RentServ {
+
+    async AcceptRent(){
+
+    }
+
+    async DeclineRent(){
+
+    }
+
+    async RequestRent() {
+
+    }
+
+    async CompleteRent() {
+
+    }
+
+}
+
+module.exports = RentServ;
